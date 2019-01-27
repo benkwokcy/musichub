19fd0586e2b0caefc7c98810e6aac216d7cd0ea4
James Zang <jameslzang@hotmail.com>
2019-01-27 06:49:27 -0800

    Add header navbar

---
+++ src/header/header.js
@@ -0,0 +1,26 @@
+const {dialog} = require("electron").remote;
+
+$('#currentFile').ready(() => {
+  let configFilePath = require('path').normalize(remote.app.getPath("appData") + "/musichub/configuration.json");
+  let config = JSON.parse(fs.readFileSync(configFilePath, "utf8"));
+  let fileName = "No file selected"
+  if (config.lastScore) {
+    path = config.lastScore;
+    fileName = path.replace(/^.*[\\\/]/, '');
+  }
+  $('#currentFile').html(fileName);
+})
+
+$('#headerButton').click(() => {
+  let path = dialog.showOpenDialog({
+    defaultPath: require('path').normalize(remote.app.getPath("documents") + "/musichub"),
+    properties: ['openFile']
+  })[0];
+  let configFilePath = require('path').normalize(remote.app.getPath("appData") + "/musichub/configuration.json");
+  let config = JSON.parse(fs.readFileSync(configFilePath, "utf8"));
+  config.lastScore = path;
+  fs.writeFileSync(configFilePath, JSON.stringify(config));
+  let fileName = path.replace(/^.*[\\\/]/, '');
+  $('#currentFile').html(fileName);
+  // trigger git graph
+});
